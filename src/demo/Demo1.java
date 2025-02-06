9b40f19a0ed6aa8db679c0d6e6e0886233977ae8
Acer <Acer@DESKTOP-3ELFCHO>
2025-02-07 00:15:12 +0100

    Solucionado conflicto cambio3

--- src/demo/Demo1.java
+++ src/demo/Demo1.java
@@ -5,11 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Linea 1");
-<<<<<<< HEAD
-		System.out.println("Linea 2 en conflicto");
-=======
-		System.out.println("Linea 2 pero en master");
->>>>>>> refs/heads/master
+		System.out.println("Linea 2 SOLUCIONADO");
 		System.out.println("Linea 3");
 	   //Estado antes del merge
 	}
