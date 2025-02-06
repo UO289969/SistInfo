f16944d9b091fe68ce21653166ea775bb8cd5e59
Acer <Acer@DESKTOP-3ELFCHO>
2025-02-07 00:06:55 +0100

    Linea 2 pero en master

--- src/demo/Demo1.java
+++ src/demo/Demo1.java
@@ -5,7 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Linea 1");
-		System.out.println("Linea 2");
+		System.out.println("Linea 2 pero en master");
 		System.out.println("Linea 3");
 	   //Estado antes del merge
 	}
