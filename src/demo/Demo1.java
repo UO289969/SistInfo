0ac35e59649f58ed33c78a733b35581053c73803
Acer <Acer@DESKTOP-3ELFCHO>
2025-02-09 13:55:54 +0100

    ejercicio 3

--- src/demo/Demo1.java
+++ src/demo/Demo1.java
@@ -5,7 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Linea 1");
-		System.out.println("Linea 2 SOLUCIONADO");
+		System.out.println("Linea 2 ejercicio 3");
 		System.out.println("Linea 3");
 	   //Estado antes del merge
 	}
